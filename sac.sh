f734e24c579f869bf6bdafbb2828287f4655e7ff
heiqiu47 <64836651+heiqiu47@users.noreply.github.com>
2024-01-01 18:37:57 +0800

    Add files via upload

--- sac.sh
+++ sac.sh
@@ -6,18 +6,26 @@ hopingmiao=hotmiao
 function clewdSettings { 
     clewd_dir=clewd
     # 3. Clewd设置
-    echo -e "\033[0;36mhoping：选一个执行喵~\n\033[0m\033[0;33m--------------------------------------\n\033[0m\033[0;33m选项1 为Clewd添加Cookies\n\033[0m\033[0;37m选项2 修改Clewd密码\n\033[0m\033[0;33m选项3 修改Clewd端口\n\033[0m\033[0;37m选项4 修改 Cookiecounter(Cookie切换频率)\n\033[0m\033[0;33m选项0 删除现有clewd，下载最新测试修改版clewd\n\033[0m\033[0;33m--------------------------------------\n\033[0m"
+    echo -e "\033[0;36mhoping：选一个执行喵~\n\033[0m\033[0;33m--------------------------------------\n\033[0m\033[0;33m选项1 查看 config.js 配置文件\n\033[0m\033[0;37m选项2 使用 Vim 编辑 config.js\n\033[0m\033[0;33m选项3 为 Clewd 添加 Cookies\n\033[0m\033[0;37m选项4 修改 Clewd 密码\n\033[0m\033[0;33m选项5 修改 Clewd 端口\n\033[0m\033[0;37m选项6 修改 Cookiecounter\n\033[0m\033[0;33m选项7 修改 rProxy\n\033[0m\033[0;37m选项0 删除现有clewd，下载最新测试修改版clewd\n\033[0m\033[0;33m--------------------------------------\n\033[0m"
     read -n 1 option
     echo
     case $option in 
         1) 
-            # 为 Clewd 添加 Cookies
+            # 查看 config.js
+            cat $clewd_dir/config.js
+            ;;
+        2)
+            # 使用 Vim 编辑 config.js
+            vim $clewd_dir/config.js
+            ;;
+        3) 
+            # 添加 Cookies
             echo "hoping：请输入你的cookie文本喵~(回车进行保存，如果全部输入完后按一次ctrl+D即可退出输入):"
             while IFS= read -r line; do
                 cookies=$(echo "$line" | grep -E -o '"?sessionKey=[^"]{100,120}AA"?' | tr -d "\"'")
                 echo "$cookies"
                 if [ -n "$cookies" ]; then
-                    echo "我猜你的cookies是:\n"
+                    echo -p "我猜你的cookies是:\n"
                     echo "$cookies"
                     # Format cookies, one per line with quotes
                     cookies=$(echo "$cookies" | tr ' ' '\n' | sed -e 's/^/"/; s/$/"/g')
@@ -30,9 +38,9 @@ function clewdSettings {
                     echo "没有找到cookie喵~o(╥﹏╥)o，要不检查一下cookie是不是输错了吧？(如果要退出输入请按Ctrl+D)"
                 fi
             done
-            echo "cookies成功输入了，(*^▽^*)"
+            echo "cookies成功输入了(*^▽^*)"
             ;;
-        2) 
+        4) 
             # 修改 Clewd 密码
             read -p "是否修改密码?(y/n)" choice
             if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
@@ -47,7 +55,7 @@ function clewdSettings {
                 echo "未修改密码"
             fi
             ;; 
-        3) 
+        5) 
             # 修改 Clewd 端口
             read -p "是否要修改开放端口?(y/n)" choice
             if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
@@ -61,7 +69,7 @@ function clewdSettings {
                 echo "未修改端口号"
             fi
             ;;
-        4)  
+        6)  
             # 修改 Cookiecounter
             echo "切换Cookie的频率, 默认为3(每3次切换), 改为-1进入测试Cookie模式"
             read -p "是否要修改Cookiecounter?(y/n)" choice
@@ -76,6 +84,29 @@ function clewdSettings {
                 echo "未修改Cookiecounter"
             fi
             ;;
+        7)  
+            # 修改 rProxy
+            echo -p "1. 官网地址claude.ai\n2. 国内镜像地址finechat.ai\n0.不修改\n"
+            read -p "输入选择喵：" choice
+            case $choice in 
+                1) 
+                    sed -i 's/"rProxy": */"rProxy": ''/g' $clewd_dir/config.js
+                    ;; 
+                2) 
+                    sed -i 's/"rProxy": */"rProxy": 'https://chat.finechat.ai'/g' $clewd_dir/config.js
+                    ;; 
+                3)
+                    # 读取用户输入rProxy
+                    read -p "请输入需要设置的数值:" rProxy
+                    # 更新配置文件的rProxy
+                    sed -i 's/"rProxy": [0-9]*/"rProxy": '$rProxy'/g' $clewd_dir/config.js
+                    echo "rProxy已修改为$rProxy"
+                    ;; 
+                0) 
+                    echo "不修改喵~"
+                    break ;; 
+            esac
+            ;;    
         0)
             cd /root
 	        rm -rf clewd
@@ -218,16 +249,15 @@ if  [ ! -d "clewd" ] || [ ! -f "clewd/config.js" ]; then
 	exit 3
 fi
 
+# 主菜单
 echo "                                              
-烧猫一键安卓脚本
-作者：hoping喵
+烧猫一键脚本
+作者：hoping喵，水秋喵
 版本：$version
 来自：Claude2.1先行破限组
-群号：704819371，910524479，304690608             
-类脑Discord(角色卡发布等): https://discord.gg/HWNkueX34q 
-此程序完全免费，不允许如浅睡一天一夜等人对脚本与教程进行盗用/商用。运行时需要稳定的魔法网络环境。"
-
-# 主菜单
+群号：704819371，910524479，304690608
+类脑Discord(角色卡发布等): https://discord.gg/HWNkueX34q
+此程序完全免费，不允许如浅睡一天一夜等人对脚本/教程进行盗用/商用。运行时需要稳定的魔法网络环境。"
 while :
 do 
     echo -e "\033[0;36mhoping喵~让你选一个执行（输入数字即可），懂了吗？\033[0;38m(｡ì _ í｡)\033[0m\n\033[0m\033[0;33m--------------------------------------\n\033[0m\033[0;31m选项0 退出脚本\n\033[0m\033[0;33m选项1 启动Clewd\n\033[0m\033[0;37m选项2 启动酒馆\n\033[0m\033[0;33m选项3 Clewd设置\n\033[0m\033[0;37m选项4 酒馆配置\n\033[0m\033[0;31m选项5 更新脚本\n\033[0m\033[0;33m--------------------------------------\n\033[0m\033[0;35m不准选其他选项，听到了吗？\n\033[0m\n(⇀‸↼‶)"
@@ -242,12 +272,15 @@ do
             echo "端口为$port, 出现 (x)Login in {邮箱} 代表启动成功喵, 后续出现AI无法应答等报错请参考本窗口。"
             cd clewd
             bash start.sh
-            echo "Clewd已关闭, 返回主菜单"
+            echo "Clewd已关闭, 即将返回主菜单"
+            cd ../
             ;; 
         2) 
             #启动SillyTavern
             cd SillyTavern
 	        bash start.sh
+            echo "酒馆已关闭, 即将返回主菜单"
+            cd ../
             ;; 
         3) 
             #Clewd设置
@@ -267,5 +300,5 @@ do
             ;;
     esac
 done 
-echo "已退出hot喵的一键脚本，输入 bash sac.sh 可重新进入脚本喵~"
+echo "已退出烧猫一键脚本，输入 bash sac.sh 可重新进入脚本喵~"
 exit
